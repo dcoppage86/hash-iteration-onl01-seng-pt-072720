2160e568681b4c44c1c108a496f199178172eb02
Dakota Coppage <dakotakcoppage@me.com>
2020-08-09 19:36:28 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,11 +7,13 @@
 #}
 
 def select_winner(passengers)
-winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
+  winner = ""
+  passengers.each do |suite,name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
-  select_winner
+  winner
 end
 
 
